e8ae45872f0f55350ffbd507432eb6ee165b83fb
Josh Perez <josh@goatslacker.com>
2013-05-09 21:11:05 -0700

    infinite bios

--- instabio.js
+++ instabio.js
@@ -1,108 +1,118 @@
-var whoIAm = [
-  'reader'
-, 'writer'
-, 'smoker'
-, 'swimmer'
-, 'listener'
-, 'creator'
-, 'explorer'
-, 'runner'
-, 'thinker'
-, 'communicator'
-, 'entrepeneur'
-]
-
-var descriptor = [
-  'avid'
-, 'extreme'
-, 'incurable'
-, 'evil'
-, 'hardcore'
-, 'infuriatingly'
-, 'humble'
-, 'lifelong'
-, 'friendly'
-, 'general'
-, 'total'
-, 'devoted'
-, 'typical'
-, 'award-winning'
-, 'certified'
-, 'freelance'
-]
-
-var additionally = [
-  'introvert'
-, 'extrovert'
-, 'webaholic'
-, 'student'
-, 'organizer'
-]
-
-var reallyCoolThing = [
-, 'beer'
-, 'coffee'
-, 'bacon'
-, 'web'
-, 'zombie'
-, 'ninja'
-, 'pirate'
-, 'anime'
-, 'music'
-, 'TV'
-, 'internet'
-, 'pop culture'
-, 'alcohol'
-, 'food'
-, 'travel'
-]
-
-var secondaryDescriptor = [
-  'nerd'
-, 'junkie'
-, 'maven'
-, 'dork'
-, 'geek'
-, 'enthusiast'
-, 'lover'
-, 'fanatic'
-, 'expert'
-, 'guru'
-, 'wizard'
-, 'scholar'
-, 'evangelist'
-, 'buff'
-, 'fan'
-]
-
-var cuteQuotes = [
-  'unable to type with boxing gloves on'
-]
-
-var randomishSort = function (a, b) { return Math.random() - 0.5 }
-
-var capitalize = function (str) {
-  return str.charAt(0).toUpperCase() + str.slice(1)
-}
-
-function getrand(topic) {
-  return topic.sort(randomishSort).shift()
-}
-
-function instabio() {
-  return [
-    [descriptor, reallyCoolThing, secondaryDescriptor],
-    [descriptor, whoIAm],
-    [descriptor, reallyCoolThing, secondaryDescriptor],
-    [additionally.concat(whoIAm, cuteQuotes)]
+(function (exports) {
+  var whoIAm = [
+    'reader'
+  , 'writer'
+  , 'smoker'
+  , 'swimmer'
+  , 'listener'
+  , 'creator'
+  , 'explorer'
+  , 'runner'
+  , 'thinker'
+  , 'communicator'
+  , 'entrepeneur'
   ]
-  .map(function (sentence) {
-    return capitalize(sentence.map(getrand).join(' '))
-  })
-  .sort(randomishSort)
-  .join('. ')
-}
 
-console.log(instabio())
+  var descriptor = [
+    'avid'
+  , 'extreme'
+  , 'incurable'
+  , 'evil'
+  , 'hardcore'
+  , 'infuriatingly'
+  , 'humble'
+  , 'lifelong'
+  , 'friendly'
+  , 'general'
+  , 'total'
+  , 'devoted'
+  , 'typical'
+  , 'award-winning'
+  , 'certified'
+  , 'freelance'
+  ]
 
-module.exports = instabio
+  var additionally = [
+    'introvert'
+  , 'extrovert'
+  , 'webaholic'
+  , 'student'
+  , 'organizer'
+  ]
+
+  var reallyCoolThing = [
+  , 'beer'
+  , 'coffee'
+  , 'bacon'
+  , 'web'
+  , 'zombie'
+  , 'ninja'
+  , 'pirate'
+  , 'anime'
+  , 'music'
+  , 'TV'
+  , 'internet'
+  , 'pop culture'
+  , 'alcohol'
+  , 'food'
+  , 'travel'
+  ]
+
+  var secondaryDescriptor = [
+    'nerd'
+  , 'junkie'
+  , 'maven'
+  , 'dork'
+  , 'geek'
+  , 'enthusiast'
+  , 'lover'
+  , 'fanatic'
+  , 'expert'
+  , 'guru'
+  , 'wizard'
+  , 'scholar'
+  , 'evangelist'
+  , 'buff'
+  , 'fan'
+  ]
+
+  var cuteQuotes = [
+    'unable to type with boxing gloves on'
+  ]
+
+  var randomishSort = function (a, b) { return Math.random() - 0.5 }
+
+  var capitalize = function (str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  function getrand(topic) {
+    return topic.sort(randomishSort).shift()
+  }
+
+  function instabio() {
+    var _descriptor = descriptor.slice(0)
+    var _reallyCoolThing = reallyCoolThing.slice(0)
+    var _secondaryDescriptor = secondaryDescriptor.slice(0)
+    var _whoIAm = whoIAm.slice(0)
+    var _additionally = additionally.concat(whoIAm, cuteQuotes)
+
+    return [
+      [_descriptor, _reallyCoolThing, _secondaryDescriptor],
+      [_descriptor, _whoIAm],
+      [_descriptor, _reallyCoolThing, _secondaryDescriptor],
+      [_additionally]
+    ]
+    .map(function (sentence) {
+      return capitalize(sentence.map(getrand).join(' '))
+    })
+    .sort(randomishSort)
+    .join('. ')
+  }
+
+  if (typeof module !== 'undefined') {
+    module.exports = instabio
+  } else {
+    exports.instabio = instabio
+  }
+}(this));
